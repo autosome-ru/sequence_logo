be3aef4a02dd29ec0a78c74773f36ed4276a388e
Ilya Vorontsov <prijutme4ty@gmail.com>
2012-08-14 15:06:40 +0400

    ppm supporting methods moved in ytilib

--- lib/sequence_logo/pmflogo_lib.rb
+++ lib/sequence_logo/pmflogo_lib.rb
@@ -8,16 +8,6 @@ class Object
 end
 
 class PPM
-  attr_accessor :name
-  
-  def get_ppm
-    self
-  end
-  
-  def get_line(v)
-    ( (v - icd4of4) / icd4of4 ).abs
-  end
-  
   def get_logo(icd_mode)
     if icd_mode == :weblogo
       get_logo_weblogo
@@ -65,22 +55,6 @@ class PPM
   end
 end
 
-def get_ppm_from_file(in_file_name)
-  case File.ext_wo_name(in_file_name)
-  when 'pat', 'pcm'
-    pm = PM.load(in_file_name)
-    pm.fixwc  if pm.words_count
-  when 'mfa', 'fasta', 'plain'
-    pm = PM.new_pcm(Ytilib.read_seqs2array(in_file_name))
-  when 'xml'
-    pm = PM.from_bismark(Bismark.new(in_file_name).elements["//PPM"])
-  when in_file_name
-    pm = PPM.from_IUPAC(in_file_name.upcase)
-  end
-  pm.get_ppm
-rescue
-  nil
-end
 
 def create_canvas(ppm, options)
   x_size = options[:x_unit] * ppm.length
