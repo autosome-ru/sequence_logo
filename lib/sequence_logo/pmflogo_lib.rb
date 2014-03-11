2e7e2d3f8e4a3eef618053a6bbb75d101685df06
Ilya Vorontsov <prijutme4ty@gmail.com>
2014-03-11 17:54:22 +0400

    rewrite snp drawing so that order of nucleotides depends on input

--- lib/sequence_logo/pmflogo_lib.rb
+++ lib/sequence_logo/pmflogo_lib.rb
@@ -7,6 +7,7 @@ module SequenceLogo
     attr_reader :letter_images
     def x_size; x_unit * length; end
     def y_size; y_unit; end
+
     def initialize(length, letter_images, options = {})
       @length = length
       @x_unit = options[:x_unit] || 30
@@ -14,34 +15,44 @@ module SequenceLogo
       @letter_images = letter_images
 
       @i_logo = Magick::ImageList.new
-      @i_logo.new_image(x_size, y_size, Magick::HatchFill.new('white', 'white'))
+      @i_logo.new_image(x_size, y_size){ self.background_color = 'transparent' }
+      @shift = 0
+      yield self  if block_given?
     end
 
-    def background(bg)
-      i_logo.new_image(x_size, y_size, bg)
+    def background(fill)
+      @i_logo.unshift Magick::Image.new(x_size, y_size, fill)
     end
 
-    # Takes an array with letter heights and draws a logo position with tall to short letters at each position
-    def draw_letters_for_positon(position)
+    # Takes an enumerable with count=>letter pairs draws a logo position with letters in order of enumeration
+    def logo_for_position_ordered(letters_by_counts)
       position_logo = Magick::ImageList.new
       position_logo.new_image(x_unit, y_unit){ self.background_color = 'transparent' }
       y_pos = 0
-      # sort by [count, letter_index] allows us to make stable sort (it's useful for predictable order of same-height nucleotides)
-      position.each_with_index.sort_by{|count, letter_index| [count, letter_index] }.reverse_each do |count, letter_index|
+      letters_by_counts.each do |count, letter_index|
         next  if y_unit * count <= 1
         y_block = (y_unit * count).round
         y_pos += y_block
         position_logo << letter_image(letter_index, x_unit, y_block)
         position_logo.cur_image.page = Magick::Rectangle.new(0, 0, 0, y_unit - y_pos)
       end
-      position_logo
+      position_logo.flatten_images
+    end
+
+    # add logo of position to subsequent position
+    def add_position_logo(logo)
+      @i_logo << logo
+      @i_logo.cur_image.page = Magick::Rectangle.new(0, 0, @shift * x_unit, 0)
+      @shift += 1
+    end
+
+    # Takes an array with letter heights and draws a logo position with tall to short letters at each position
+    def logo_for_positon(position)
+      add_position_logo(logo_for_position_ordered( position_sorted_by_count(position) ))
     end
 
     def draw_logo(logo_matrix)
-      logo_matrix.each_with_index do |position, i|
-        @i_logo << draw_letters_for_positon(position).flatten_images
-        @i_logo.cur_image.page = Magick::Rectangle.new(0, 0, i * x_unit, 0)
-      end
+      logo_matrix.each{|position|  logo_for_positon(position)  }
     end
 
     def letter_image(letter_index, x_size, y_size)
@@ -64,10 +75,12 @@ module SequenceLogo
     def logo
       @i_logo.flatten_images
     end
-  end
 
-  def self.letter_image(images, letter_index, x_size, y_size)
-    images[letter_index].dup.resize(x_size, y_size)
+    # [3,1,1,2] ==> [[3, 0],[2, 3],[1, 1],[1, 2]] (derived from [[3, 'A'],[2,'T'],[1,'C'],[1,'G']])
+    def position_sorted_by_count(position)
+      # sort by [count, letter_index] allows us to make stable sort by count (it's useful for predictable order of same-height nucleotides)
+      position.each_with_index.sort_by{|count, letter_index| [count, letter_index] }.reverse
+    end
   end
 
   def self.letter_images(scheme_dir)
@@ -92,57 +105,53 @@ module SequenceLogo
 
     scheme_dir = File.join(AssetsPath, options[:scheme])
     letter_images = letter_images(scheme_dir)
-    canvas = LogoCanvas.new(ppm.length, letter_images, x_unit: options[:x_unit], y_unit: options[:y_unit])
-
-    if options[:icd_mode] == :discrete
-      canvas.background(Magick::HatchFill.new('white', 'white'))
-      if options[:threshold_lines]
-        canvas.draw_threshold_line(ppm.get_line(ppm.icd2of4))
-        canvas.draw_threshold_line(ppm.get_line(ppm.icdThc))
-        canvas.draw_threshold_line(ppm.get_line(ppm.icdTlc))
+    LogoCanvas.new(ppm.length, letter_images, x_unit: options[:x_unit], y_unit: options[:y_unit]) do |canvas|
+      if options[:icd_mode] == :discrete
+        canvas.background(Magick::HatchFill.new('white', 'white'))
+        if options[:threshold_lines]
+          canvas.draw_threshold_line(ppm.get_line(ppm.icd2of4))
+          canvas.draw_threshold_line(ppm.get_line(ppm.icdThc))
+          canvas.draw_threshold_line(ppm.get_line(ppm.icdTlc))
+        end
+      else
+        canvas.background(Magick::HatchFill.new('white', 'bisque'))
       end
-    else
-      canvas.background(Magick::HatchFill.new('white', 'bisque'))
-    end
-    canvas.draw_logo( ppm.get_logo(options[:icd_mode]) )
-    canvas.logo
+      canvas.draw_logo( ppm.get_logo(options[:icd_mode]) )
+    end.logo
   end
 
   def self.draw_sequence_logo(sequence, options = {})
-    logo = logo_by_sequence(sequence)
     scheme_dir = File.join(AssetsPath, options[:scheme])
     letter_images = letter_images(scheme_dir)
-    canvas = LogoCanvas.new(sequence.length, letter_images, x_unit: options[:x_unit], y_unit: options[:y_unit])
-    canvas.background(Magick::HatchFill.new('white', 'white'))
-    canvas.draw_logo(logo)
-    canvas.logo
+    LogoCanvas.new(sequence.length, letter_images, x_unit: options[:x_unit], y_unit: options[:y_unit]) do |canvas|
+      canvas.background(Magick::HatchFill.new('white', 'white'))
+      canvas.draw_logo( logo_matrix_by_sequence(sequence) )
+    end.logo
   end
 
   def self.draw_sequence_w_snp_logo(sequence_w_snp, options = {})
-    logo = logo_by_sequence_w_snp(sequence_w_snp)
+    left, mid, right = sequence_w_snp.split(/[\[\]]/)
+    allele_variants = mid.split('/').map(&:upcase)
+    snp_counts = allele_variants.map{|letter| [1.0 / allele_variants.size, letter_index(letter)] }
+
     scheme_dir = File.join(AssetsPath, options[:scheme])
     letter_images = letter_images(scheme_dir)
-    canvas = LogoCanvas.new(logo.length, letter_images, x_unit: options[:x_unit], y_unit: options[:y_unit])
-    canvas.background(Magick::HatchFill.new('white', 'white'))
-    canvas.draw_logo(logo)
-    canvas.logo
+    LogoCanvas.new(left.length + 1 + right.length, letter_images, x_unit: options[:x_unit], y_unit: options[:y_unit]) do |canvas|
+      canvas.background(Magick::HatchFill.new('white', 'white'))
+
+      logo_matrix_by_sequence(left).each{|position|  canvas.logo_for_positon(position)  }
+      canvas.add_position_logo(canvas.logo_for_position_ordered(snp_counts))
+      logo_matrix_by_sequence(right).each{|position|  canvas.logo_for_positon(position)  }
+    end.logo
   end
 
-  def self.logo_by_sequence(sequence)
-    sequence.each_char.map{|letter| {'A' => 0 ,'C' => 1,'G' => 2 ,'T' => 3}[letter.upcase] }.map{|letter_index| 4.times.map{|i| i == letter_index ? 1.0 : 0.0 }}
+  def self.letter_index(letter)
+    {'A' => 0 ,'C' => 1,'G' => 2 ,'T' => 3}[letter.upcase]
   end
 
-  def self.logo_of_snp_position(allele_variants)
-    allele_variants = allele_variants.map(&:upcase)
-    ['A','C','G','T'].map{|letter| allele_variants.include?(letter) ? (1.0 / allele_variants.size) : 0.0 }
+  def self.logo_matrix_by_sequence(sequence)
+    sequence.each_char.map{|letter| letter_index(letter) }.map{|letter_index| 4.times.map{|i| i == letter_index ? 1.0 : 0.0 }}
   end
-
-  def self.logo_by_sequence_w_snp(sequence_w_snp)
-    left, mid, right = sequence_w_snp.split(/[\[\]]/)
-    logo_by_sequence(left) + [ logo_of_snp_position(mid.split('/')) ] + logo_by_sequence(right)
-    # sequence.each_char.map{|letter| {'A' => 0 ,'C' => 1,'G' => 2 ,'T' => 3}[letter.upcase] }.map{|letter_index| 4.times.map{|i| i == letter_index ? 1.0 : 0.0 }}
-  end
-
 
   # logos = { filename => {shift: ..., length: ..., name: ...} }
   def self.glue_files(logos, output_file, options)
