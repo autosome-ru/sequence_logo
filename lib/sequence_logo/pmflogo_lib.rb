5b3c8899807c15678244ff28a5d404b89a84def9
Ilya Vorontsov <prijutme4ty@gmail.com>
2014-03-11 14:29:30 +0400

    exract logo canvas into separate class

--- lib/sequence_logo/pmflogo_lib.rb
+++ lib/sequence_logo/pmflogo_lib.rb
@@ -2,41 +2,59 @@ require_relative 'ytilib'
 require 'RMagick'
 
 module SequenceLogo
-  def self.draw_threshold_lines(i_logo, ppm)
-    x_size = i_logo.columns
-    y_size = i_logo.rows
+  class LogoCanvas
+    attr_reader :i_logo, :x_unit, :y_unit, :length
+    attr_reader :letter_images
+    def x_size; x_unit * length; end
+    def y_size; y_unit; end
+    def initialize(length, letter_images, options = {})
+      @length = length
+      @x_unit = options[:x_unit] || 30
+      @y_unit = options[:y_unit] || 60
+      @letter_images = letter_images
 
-    line2of4 = y_size - ppm.get_line(ppm.icd2of4) * y_size
-    lineThc = y_size - ppm.get_line(ppm.icdThc) * y_size
-    lineTlc = y_size - ppm.get_line(ppm.icdTlc) * y_size
-
-    dr = Magick::Draw.new
-    dr.fill('transparent')
-
-    dr.stroke_width(y_size / 200.0)
-    dr.stroke_dasharray(7,7)
-
-    dr.stroke('silver')
-    dr.line(0, line2of4, x_size, line2of4)
-    dr.line(0, lineThc, x_size, lineThc)
-    dr.line(0, lineTlc, x_size, lineTlc)
-
-    dr.draw(i_logo)
-  end
-
-  def self.create_canvas(ppm, options)
-    x_size = options[:x_unit] * ppm.length
-    y_size = options[:y_unit]
-
-    i_logo = Magick::ImageList.new
-    if options[:icd_mode] == :discrete
-      i_logo.new_image(x_size, y_size, Magick::HatchFill.new('white', 'white'))
-      draw_threshold_lines(i_logo, ppm)  if options[:threshold_lines]
-    else
-      i_logo.new_image(x_size, y_size, Magick::HatchFill.new('white', 'bisque'))
+      @i_logo = Magick::ImageList.new
+      @i_logo.new_image(x_size, y_size, Magick::HatchFill.new('white', 'white'))
     end
 
-    i_logo
+    def background(bg)
+      i_logo.new_image(x_size, y_size, bg)
+    end
+
+    # Takes a matrix with letter heights and draws a logo with tall to short letters at each position
+    def draw_logo(logo_matrix)
+      logo_matrix.each_with_index do |position, i|
+        y_pos = 0
+        position.each_with_index.sort_by{|count, letter_index| count }.reverse_each do |count, letter_index|
+          next  if y_unit * count <= 1
+          y_block = (y_unit * count).round
+          y_pos += y_block
+          @i_logo << letter_image(letter_index, x_unit, y_block)
+          @i_logo.cur_image.page = Magick::Rectangle.new(0, 0, i * x_unit, y_unit - y_pos)
+        end
+      end
+    end
+
+    def letter_image(letter_index, x_size, y_size)
+      letter_images[letter_index].dup.resize(x_size, y_size)
+    end
+
+    def draw_threshold_line(threshold_level)
+      y_coord = y_size - threshold_level * y_size
+      dr = Magick::Draw.new
+      dr.fill('transparent')
+
+      dr.stroke_width(y_size / 200.0)
+      dr.stroke_dasharray(7,7)
+
+      dr.stroke('silver')
+      dr.line(0, y_coord, x_size, y_coord)
+      dr.draw(@i_logo)
+    end
+
+    def logo
+      @i_logo.flatten_images
+    end
   end
 
   def self.letter_image(images, letter_index, x_size, y_size)
@@ -56,32 +74,29 @@ module SequenceLogo
     Magick::ImageList.new(*letter_files)
   end
 
-  # Takes a matrix with letter heights and draws a logo with tall to short letters at each position
-  def self.draw_letters_on_canvas(i_logo, i_letters, matrix, options)
-    y_unit = options[:y_unit]
-    x_unit = options[:x_unit]
-    matrix.each_with_index do |position, i|
-      y_pos = 0
-      position.each_with_index.sort_by{|count, letter_index| count }.reverse_each do |count, letter_index|
-        next  if y_unit * count <= 1
-        y_block = (y_unit * count).round
-        y_pos += y_block
-        i_logo << letter_image(i_letters, letter_index, x_unit, y_block)
-        i_logo.cur_image.page = Magick::Rectangle.new(0, 0, i * x_unit, y_unit - y_pos )
-      end
-    end
-  end
-
   def self.draw_logo(ppm, options = {})
     ppm.words_count = options[:words_count]  if options[:words_count]
     unless ppm.words_count
       report "words count for PPM is undefined, assuming weblogo mode"
       options[:icd_mode] = :weblogo
     end
-    i_logo = create_canvas(ppm, options)
+
     scheme_dir = File.join(AssetsPath, options[:scheme])
-    draw_letters_on_canvas(i_logo, letter_images(scheme_dir), ppm.get_logo(options[:icd_mode]), options)
-    i_logo = i_logo.flatten_images
+    letter_images = letter_images(scheme_dir)
+    canvas = LogoCanvas.new(ppm.length, letter_images, x_unit: options[:x_unit], y_unit: options[:y_unit])
+
+    if options[:icd_mode] == :discrete
+      canvas.background(Magick::HatchFill.new('white', 'white'))
+      if options[:threshold_lines]
+        canvas.draw_threshold_line(ppm.get_line(ppm.icd2of4))
+        canvas.draw_threshold_line(ppm.get_line(ppm.icdThc))
+        canvas.draw_threshold_line(ppm.get_line(ppm.icdTlc))
+      end
+    else
+      canvas.background(Magick::HatchFill.new('white', 'bisque'))
+    end
+    canvas.draw_logo( ppm.get_logo(options[:icd_mode]) )
+    canvas.logo
   end
 
   # logos = { filename => {shift: ..., length: ..., name: ...} }
